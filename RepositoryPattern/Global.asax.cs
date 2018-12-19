41f7433d962e38115f3ac8b3241ef941737958bc
a <a>
2018-12-19 14:17:49 +0530

    Add project files.

---
+++ RepositoryPattern/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace RepositoryPattern
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected  void Session_Start()
+        {
+            
+        }
+        protected void Application_BeginRequest()
+        {
+
+        }
+        protected void Application_AuthenticateRequest()
+        {
+
+        }
+        protected void Application_EndRequest()
+        {
+
+        }
+        protected void Session_End()
+        {
+
+        }
+        protected void Application_End()
+        {
+
+        }
+        protected void Application_Error()
+        {
+
+        }
+
+    }
+}
